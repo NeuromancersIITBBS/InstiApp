dff603ca5beab878cec98bbc8b251c39c8e0c0fb
Zoham <sohamk97@gmail.com>
2019-01-16 12:09:04 +0530

    Gymkhana Officer Bearers Page Added

--- app/src/main/java/neuromancers/iitbbs/iitbhubaneswar/MainActivity.java
+++ app/src/main/java/neuromancers/iitbbs/iitbhubaneswar/MainActivity.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -23,12 +22,6 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity
@@ -129,10 +122,10 @@ public class MainActivity extends AppCompatActivity
                 setTitle("Campus Map");
                 setNavFragment(R.layout.map);
                 break;
-//            case R.id.nav_gymkhana:
-//                setTitle("Students' Gymkhana");
-//                setNavFragment(R.layout.gymkhana);
-//                break;
+            case R.id.nav_gymkhana:
+                setTitle("Students' Gymkhana Office");
+                setNavFragment(R.layout.gymkhana);
+                break;
             case R.id.nav_timetable:
                 setTitle("Academic Time Table");
                 setNavFragment(R.layout.timetable);
